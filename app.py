6ab23da700a7099ffc15ef89578a173370c6df16
Miraj <106034242+MirajMalik@users.noreply.github.com>
2025-02-06 19:14:55 +0000

    added html and css

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask     #import Flask Class
+from flask import Flask,render_template    #import Flask Class
 
 app = Flask(__name__)       # create app object
 
 @app.route('/')              #create a page on the first url
 
 def hello_world():
-  return "Hello, Miraj"
+  return render_template('home.html')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug = True)
